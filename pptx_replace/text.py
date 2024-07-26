5f914954d4ebe8f738dc896db54231fdc0ab03f9
John Lyu <lvjunhong@citics.com>
2024-07-26 13:17:57 +0800

    fix replace table lost style

--- pptx_replace/text.py
+++ pptx_replace/text.py
@@ -12,19 +12,20 @@ def delete_paragraph(paragraph):
 def set_frame_text(text_frame: TextFrame, text: str, font: Font = None):
     """set text and keep font style"""
     replaced = False
-    for paragraph in text_frame.paragraphs:
-        # use style of the first run
-        if paragraph.runs:
-            if not replaced:
-                paragraph.runs[0].text = text
-                # remove text in other runs
-                replaced = True
-                for run in paragraph.runs[1:]:
-                    run.text = ""
+    if text_frame.paragraphs:
+        for paragraph in text_frame.paragraphs:
+            # use style of the first run
+            if paragraph.runs:
+                if not replaced:
+                    paragraph.runs[0].text = text
+                    # remove text in other runs
+                    replaced = True
+                    for run in paragraph.runs[1:]:
+                        run.text = ""
+                else:
+                    delete_paragraph(paragraph)
             else:
-                delete_paragraph(paragraph)
-        else:
-            paragraph.text = text
+                paragraph.text = text
     else:
         text_frame.text = text
 
